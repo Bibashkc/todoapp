44f85437269622399a73fe33e9cced7aa168d5f1
bibash <bibashkc92@gmail.com>
2019-03-11 20:54:48 +0000

    add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -19,6 +19,22 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
   
+  def edit
+    @todo = Todo.find(params[:id])
+    render :edit
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update_attributes(todo_params)
+      flash[:errors] = ["Todo was successfully updated"]
+      redirect_to todo_url(@todo)
+    else
+      flash.now[:errors] = @todo.errors.full_messages
+      render :edit
+    end
+  end
+  
   private
   def todo_params
     params.require(:todo).permit(:name, :description)
